8eb74b17a7713bcae6bb559426e3ce577216b038
pitkin <pitkin@lab1-18.eng.utah.edu>
2016-11-06 19:33:20 -0700

    pickle install

--- run.sh
+++ run.sh
@@ -2,8 +2,5 @@ chmod +x getenv.sh
 ./getenv.sh
 source virtual_env/bin/activate
 pip install nltk
-python
-import nltk
-nltk.download()
-exit()
+python install_pickle.py
 python coreference.py listfile.listfile response_files/
