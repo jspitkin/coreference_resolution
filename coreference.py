33897a8f8ab7d5f301a976bc209cdc42a4b10cfd
Curtis Clements <ctclements@gmail.com>
2016-11-28 17:50:08 -0700

    basic gender match

--- coreference.py
+++ coreference.py
@@ -41,15 +41,15 @@ def main():
         ioutil.assign_refs_for_similars(combined_list)
         ioutil.assign_refs_for_pronouns(combined_list)
 
+        # Gender assigning
+        ioutil.it_assigner(combined_list)
+        ioutil.assign_gender(combined_list)
+        ioutil.match_previous_gender(combined_list)
+
         # Assign appositives
         nps = ioutil.get_noun_phrase_positions(path, noun_phrases)
         appositives = ioutil.get_appositives(path)
         combined_list = ioutil.match_appositive_and_np(appositives, nps, combined_list)
-
-        ioutil.it_assigner(combined_list)
-
-        #for no in combined_list:
-            #print (no.noun_phrase)
 
         # Check dates
         combined_list = ioutil.assign_date_to_today(combined_list, nps)
